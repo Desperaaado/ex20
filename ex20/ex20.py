b11ca4366ace1515810ac0aac2534ef6d9d4ddf5
murphian <murphianx@gmail.com>
2019-01-14 23:50:11 +0800

    delete() error

---
+++ ex20/ex20.py
@@ -0,0 +1,141 @@
+import sys
+
+class BSTreeNode(object):
+
+    def __init__(self, parent, key, value):
+        self.left = None
+        self.right = None
+        self.parent = parent
+        self.key = key
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.parent}: ({self.key})"
+        # return f"{self.left}<-from {self.parent}: ({self.key}, {self.value})->{self.right}"
+
+class BSTree(object):
+
+    def __init__(self):
+        self.root = None
+
+    def find_last_parent_and_node(self, node, key):
+        last_parent = None
+
+        while node:
+            if key == node.key:
+                last_parent = node.parent
+                break
+            elif key < node.key:
+                last_parent = node
+                node = node.left
+            elif key > node.key:
+                last_parent = node
+                node = node.right
+            else:
+                sys.exit(1)
+
+        return last_parent, node
+
+    def set(self, key, value):
+        last_parent, node = self.find_last_parent_and_node(self.root, key)
+
+        if node:
+            node.value = value
+        elif not last_parent:
+            self.root = BSTreeNode(None, key, value)
+        else:
+            if key < last_parent.key:
+                last_parent.left = BSTreeNode(last_parent, key, value)
+            elif key > last_parent.key:
+                last_parent.right = BSTreeNode(last_parent, key, value)
+            else:
+                sys.exit(2)
+
+    def get(self, key):
+        last_parent, node = self.find_last_parent_and_node(self.root, key)
+        return node and node.value or None
+        
+    def list(self, node, i=0):
+        the_mid = []
+        if node:
+            i += 1
+            item = (node.key, node.value)
+            # parent_key = node.parent and node.parent.key or None
+            # left_key = node.left and node.left.key or None
+            # right_key = node.right and node.right.key or None
+            # print(f"No.{i}: {node.key}, parent: {parent_key}, left: {left_key}, right: {right_key}")
+            the_mid.append(item)
+            the_left = self.list(node.left, i)
+            the_right = self.list(node.right, i)
+            return the_left + the_mid + the_right
+        else:
+            return []
+
+    def show_list(self):
+        print(self.list(self.root))
+
+    def delete(self, node, key):
+        last_parent, node = self.find_last_parent_and_node(node, key)
+        if not node:
+            print(f"No.{key} isn't exist.")
+            return None
+        elif not last_parent:
+            result = self.root.value
+            if (not node.left) and (not node.right):
+                return node.value
+            elif (not node.left) and node.right:
+                node.right.parent = last_parent
+                return node.value
+            elif node.left and (not node.right):
+                node.left.parent = last_parent
+                return node.value
+            elif node.left and node.right:
+                node.key = node.left.key
+                self.delete(node.left, node.left.key)
+            else:
+                sys.exit(6)
+            return result
+
+        if (not node.left) and (not node.right):
+
+            if last_parent.left == node:
+                last_parent.left = None
+            elif last_parent.right == node:
+                last_parent.right = None
+            else:
+                sys.exit(5)
+
+            return node.value
+        elif (not node.left) and node.right:
+            node.right.parent = last_parent
+
+            if last_parent.left == node:
+                last_parent.left = node.right
+            elif last_parent.right == node:
+                last_parent.right = node.right
+            else:
+                sys.exit(5)
+            
+            return node.value
+        elif node.left and (not node.right):
+            node.left.parent = last_parent
+
+            if last_parent.left == node:
+                last_parent.left = node.left
+            elif last_parent.right == node:
+                last_parent.right = node.left
+            else:
+                sys.exit(5)
+            
+            return node.value
+        elif node.left and node.right:
+            print('**************************')
+            result = node.key
+            node.key = node.left.key
+            self.delete(node.left, node.left.key)
+            return result
+        else:
+            sys.exit(6)
+
+    def do_delete(self, key):
+        self.delete(self.root, key)
--- tests/ex20_tests.py
+++ tests/ex20_tests.py
@@ -1,5 +1,5 @@
 from nose.tools import *
-import ex20
+from ex20.ex20 import *
 
 def setup():
     print("SETUP!")
@@ -8,4 +8,21 @@ def teardown():
     print("TEAR DOWN!")
 
 def test_basic():
-    print("I RAN!")
+    states = BSTree()
+    states.set(3, 'OR')
+    states.set(8, 'FL')
+    states.set(44, 'CA')
+    states.set(0, 'NY')
+    # states.set(44, 'CA44')
+    states.set(9, 'MI')
+    states.set(30, 'OR')
+    states.set(80, 'FL')
+    # states.set(404, 'CA')
+    # states.set(401, 'NY')
+    # states.set(404, 'CA44')
+    # states.set(911, 'MI')
+    # assert_equal(states.get(44), 'CA44')
+    states.show_list()
+    states.do_delete(44)
+    # states.do_delete(3)
+    states.show_list()
